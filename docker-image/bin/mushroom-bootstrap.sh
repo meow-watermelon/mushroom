60ba00a00a16de946306248bfb9f0710e8901fa1
Hui Li <herdingcat@yahoo.com>
2022-03-13 15:54:54 -0700

    [WIP] - add MPI STREAM benchmark utility
    Signed-off-by: Hui Li <herdingcat@yahoo.com>

--- docker-image/bin/mushroom-bootstrap.sh
+++ docker-image/bin/mushroom-bootstrap.sh
@@ -7,7 +7,7 @@ echo "### Unlocking root user ... ###"
 /usr/bin/passwd -u root
 
 echo "### Installing system packages ... ###"
-/sbin/apk add --no-cache openrc openssh
+/sbin/apk add --no-cache openrc openssh build-base
 /sbin/rc-update add sshd
 /bin/rc-status
 
@@ -16,10 +16,12 @@ echo "### Installing system packages ... ###"
 /bin/mkdir -m 700 /root/.ssh
 
 echo "### Installing OpenMPI package ... ###"
-/sbin/apk add --no-cache openmpi
+/sbin/apk add --no-cache openmpi openmpi-dev
 
 echo "### Installing math libraries ... ###"
 
 echo "### Downloading STREAM package ... ###"
+/usr/bin/wget https://www.cs.virginia.edu/stream/FTP/Code/Versions/stream_mpi.c -O /root/stream_mpi.c
 
 echo "### Compiling and installing STREAM package ... ###"
+/usr/bin/mpicc -O3 -ffreestanding -openmp -mcmodel=medium -Wrestrict -DSTREAM_ARRAY_SIZE=80000000 -DNTIMES=20 -DVERBOSE /root/stream_mpi.c -o /bin/stream_mpi
